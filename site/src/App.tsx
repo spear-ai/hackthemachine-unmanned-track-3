b94bdf6d0a82a99e17bce2d141f119527dda8571
mike <mhunter@spear.AI>
2021-11-26 15:42:23 -0500

    chore: remove unnecessary boilerplate

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,52 +1,16 @@
 import {Component} from 'react';
 import {connect} from 'react-redux';
-// import {addDataToMap, wrapTo} from 'kepler.gl/actions';
-// import styled from 'styled-components';
-// import {theme} from 'kepler.gl/styles';
+
 import Kepler from './Kepler';
 
-// import sampleData, {config} from './data/sample-data';
+// todo: add  data
+// import permanentData, {config} from './data/sample-data';
 
-// import {
-//   SidebarFactory,
-//   PanelHeaderFactory,
-//   PanelToggleFactory,
-//   CustomPanelsFactory,
-//   MapPopoverFactory,
-//   injectComponents
-// } from 'kepler.gl/components';
 
-// import CustomPanelHeaderFactory from './components/panel-header';
-// import CustomSidebarFactory from './components/side-bar';
-// import CustomPanelToggleFactory from './components/panel-toggle';
-// import CustomSidePanelFactory from './components/custom-panel';
-// import CustomMapPopoverFactory from './components/custom-map-popover';
-
-// const StyledMapConfigDisplay = styled.div`
-//   position: absolute;
-//   z-index: 100;
-//   bottom: 10px;
-//   right: 10px;
-//   background-color: ${theme.sidePanelBg};
-//   font-size: 11px;
-//   width: 300px;
-//   color: ${theme.textColor};
-//   word-wrap: break-word;
-//   min-height: 60px;
-//   padding: 10px;
-// `;
-
-// Inject custom components
-// const KeplerGl = injectComponents([
-//   [SidebarFactory, CustomSidebarFactory],
-//   [PanelHeaderFactory, CustomPanelHeaderFactory],
-//   [PanelToggleFactory, CustomPanelToggleFactory],
-//   [CustomPanelsFactory, CustomSidePanelFactory],
-//   [MapPopoverFactory, CustomMapPopoverFactory]
-// ]);
 
 class App extends Component {
   componentDidMount() {
+    // todo: add addDataToMap
     // this.props.dispatch(wrapTo('map1', addDataToMap({datasets: sampleData, config})));
   }
 
