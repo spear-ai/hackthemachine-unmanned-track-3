782d9851c125b39b86a0df5ff52e3199a80d8d83
Dennis Torres <dtorres@spear.ai>
2021-11-29 14:52:45 -0500

    fix: make playable map area size divisible by 4

--- model/environment/generated/README.md
+++
@@ -1,4 +0,0 @@
-# Generated environments
-
-The environments in this directory were automatically generated using `scripts/generate.py`.
-To keep things simple, we'll keep the generated files in version control.
--- model/server/neural_mmo/forge/blade/lib/material.py
+++ model/server/neural_mmo/forge/blade/lib/material.py
@@ -1,80 +1,107 @@
 from pdb import set_trace as T
 
+
 class Material:
-   harvestable = False
-   capacity    = 1
-   def __init__(self, config):
-      pass
+    harvestable = False
+    capacity = 1
 
-   def __eq__(self, mtl):
-      return self.index == mtl.index
+    def __init__(self, config):
+        pass
 
-   def __equals__(self, mtl):
-      return self == mtl
+    def __eq__(self, mtl):
+        return self.index == mtl.index
+
+    def __equals__(self, mtl):
+        return self == mtl
+
 
 class Lava(Material):
-   tex   = 'lava'
-   index = 0
+    tex = 'lava'
+    index = 0
+
 
 class Water(Material):
-   tex   = 'water'
-   index = 1
+    tex = 'water'
+    index = 1
+
 
 class Grass(Material):
-   tex   = 'grass'
-   index = 2
+    tex = 'grass'
+    index = 2
+
 
 class Scrub(Material):
-   tex = 'scrub'
-   index = 3
+    tex = 'scrub'
+    index = 3
+
 
 class Forest(Material):
-   tex   = 'forest'
-   index = 4
+    tex = 'forest'
+    index = 4
 
-   harvestable = True
-   degen       = Scrub
+    harvestable = True
+    degen = Scrub
 
-   def __init__(self, config):
-      if config.game_system_enabled('Resource'):
-         self.capacity = config.RESOURCE_FOREST_CAPACITY
-         self.respawn  = config.RESOURCE_FOREST_RESPAWN
+    def __init__(self, config):
+        if config.game_system_enabled('Resource'):
+            self.capacity = config.RESOURCE_FOREST_CAPACITY
+            self.respawn = config.RESOURCE_FOREST_RESPAWN
+
 
 class Stone(Material):
-   tex   = 'stone'
-   index = 5
+    tex = 'stone'
+    index = 5
+
 
 class Orerock(Material):
-   tex   = 'iron_ore'
-   index = 6
+    tex = 'iron_ore'
+    index = 6
 
-   harvestable = True
-   degen       = Stone
+    harvestable = True
+    degen = Stone
 
-   def __init__(self, config):
-      if config.game_system_enabled('Resource'):
-         self.capacity = config.RESOURCE_OREROCK_CAPACITY
-         self.respawn  = config.RESOURCE_OREROCK_RESPAWN
+    def __init__(self, config):
+        if config.game_system_enabled('Resource'):
+            self.capacity = config.RESOURCE_OREROCK_CAPACITY
+            self.respawn = config.RESOURCE_OREROCK_RESPAWN
+
 
 class Meta(type):
-   def __init__(self, name, bases, dict):
-      self.indices = {mtl.index for mtl in self.materials}
+    def __init__(self, name, bases, dict):
+        self.indices = {mtl.index for mtl in self.materials}
 
-   def __iter__(self):
-      yield from self.materials
+    def __iter__(self):
+        yield from self.materials
 
-   def __contains__(self, mtl):
-      if isinstance(mtl, Material):
-         mtl = type(mtl)
-      if isinstance(mtl, type):
-         return mtl in self.materials
-      return mtl in self.indices
+    def __contains__(self, mtl):
+        if isinstance(mtl, Material):
+            mtl = type(mtl)
+        if isinstance(mtl, type):
+            return mtl in self.materials
+        return mtl in self.indices
+
 
 class All(metaclass=Meta):
-   materials = {Lava, Water, Grass, Scrub, Forest, Stone, Orerock}
+    # For now, treat `Water` as the ocean and `Grass` as land.
+    # We should refactor all of the materials once we get things working.
+    #
+    # Ideas:
+    # * Rename `Lava` -> `Border`
+    # * Rename `Grass` -> `Land`
+    # * Remove `Scrub`, `Forest`, `Stone`, and `Orerock`
+    # * Remove or re-work resource harvesting of the tiles
+    materials = {Lava, Water, Grass, Scrub, Forest, Stone, Orerock}
+
 
 class Impassible(metaclass=Meta):
-   materials = {Lava, Stone, Orerock}
+    # materials = {Lava, Stone, Orerock} (neural-mmo default)
+
+    # Make land impassible for now. We'll need to reconsider this for aerial agents.
+    materials = {Grass}
+
 
 class Habitable(metaclass=Meta):
-   materials = {Grass, Scrub, Forest}
+    # materials = {Grass, Scrub, Forest} (neural-mmo default)
+
+    # Make water habitable.
+    materials = {Water}
